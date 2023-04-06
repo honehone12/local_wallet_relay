7cc227ec6db8c8ea7d027ce61cd58af3253063a4
honehone12 <marshalc@hotmail.co.jp>
2023-04-06 21:46:31 +0900

    added address request

--- client/aptos-wallet/src/App.tsx
+++ client/aptos-wallet/src/App.tsx
@@ -8,24 +8,50 @@ interface RpcRequest {
   type_arguments: string[];
 }
 
+interface Address {
+  hex: string
+}
+
 function App() {
-  const eventSource = new EventSource('sse');
+  let eventSource: EventSource | null = null;
   const [address, setAddress] = React.useState<string | null>(null);
+  const params = new URLSearchParams(window.location.search);
 
   const init = async () => {
-    eventSource.addEventListener('data', async (event) => {  
-      console.log('server sent event: ', event.data);
-      eventSource.close();
-      const payload: RpcRequest = JSON.parse(event.data);
+    const {address} = await window.aptos.connect();
+    setAddress(address);
+    
+    if (params.has('payload')) {
+      eventSource = new EventSource('sse');
+      if (eventSource !== null) {
+        eventSource.addEventListener('payload', async (event) => {  
+          console.log('server sent event: ', event.data);
+          if (eventSource !== null) {
+            eventSource.close();
+          }
+          const payload: RpcRequest = JSON.parse(event.data);
+          try {
+            await window.aptos.signAndSubmitTransaction(payload);
+          } catch (e) {
+            console.log(e);
+          }
+        });
+      }
+    }
+
+    if (params.has('address') && address !== null) {
       try {
-        await window.aptos.signAndSubmitTransaction(payload);
+        const res = await fetch('http://127.0.0.1:8080/address', {
+          method: 'POST',
+          mode: 'same-origin',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({hex: address}) 
+        });
+        console.log('response of fetch: ', res);
       } catch (e) {
         console.log(e);
       }
-    });
-    
-    const {address} = await window.aptos.connect();
-    setAddress(address);
+    }
   }
 
   React.useEffect(() => {
@@ -35,7 +61,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Connecting to wallet <code>{address}</code></p>
+        <p>
+          This is served from local computer.<br/>
+          Only your wallet has internet access.
+        </p>
       </header>
     </div>
   );
--- server/wallet_server/src/main.rs
+++ server/wallet_server/src/main.rs
@@ -3,10 +3,10 @@ use std::{
     net::{SocketAddr, Ipv4Addr},
     path::PathBuf, 
     time::Duration, 
-    sync::{Arc, RwLock},
-    env
+    sync::{Arc, Mutex}
 };
 use serde::{Serialize, Deserialize};
+use tokio::{time, sync::RwLock};
 use tokio_stream::StreamExt as _;
 use futures::{
     stream::{self, Stream}
@@ -21,7 +21,8 @@ use axum::{
     http::StatusCode,
     response::{sse::{Sse, Event}, IntoResponse}, 
     extract::State,
-    Json, Router, Server, error_handling::HandleErrorLayer 
+    Json, Router, Server, 
+    error_handling::HandleErrorLayer
 };
 
 #[derive(Serialize, Deserialize, Default, Clone)]
@@ -33,52 +34,81 @@ struct RpcPayload {
     type_arguments: Vec<String>
 }
 
+#[derive(Serialize, Deserialize, Default, Clone)]
+struct Address {
+    hex: String
+}
+
 #[derive(Serialize)]
 struct RpcResponse {
     message: String
 }
 
-type TempMemory = Arc<RwLock<RpcPayload>>;
+type TempPayloadMemory = Arc<Mutex<Option<RpcPayload>>>;
+type TempAddressMemory = Arc<RwLock<Option<Address>>>;
 
 #[tokio::main]
 async fn main() -> Result<(), hyper::Error> {
-    // default display
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_file(true).compact()
+        .with_line_number(true)
+        .init();
 
     let browser_side_addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
     let application_side_addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8081);
 
-    let memory = TempMemory::default();
-
+    let payload_memory = TempPayloadMemory::default();
+    let address_memory = TempAddressMemory::default();
+    
     // let assets_path = "./assets".to_string();
-    let assets_path = env!("DEV_SERVER_DIR").to_string();
+    let assets_path = "../../client/aptos-wallet/build".to_string();
     let assets_dir = PathBuf::from(assets_path);
     let static_files_service = ServeDir::new(assets_dir)
         .append_index_html_on_directories(true);
-    
     let browser_side_route = Router::new()
         .fallback_service(static_files_service)
-        .route("/sse", get(browser_handler))
-        .with_state(memory.clone());
-    let application_side_route = Router::new()
-        .route("/", post(application_handler))
+        .route("/sse", get(payload_sse_handler))
+        .with_state(payload_memory.clone())
+        .route("/address", post(address_fetch_handler))
+        .with_state(address_memory.clone())
         .layer(
             ServiceBuilder::new()
-                .layer(HandleErrorLayer::new(|e: BoxError| async move {
-                    if e.is::<tower::timeout::error::Elapsed>() {
-                        Ok(StatusCode::REQUEST_TIMEOUT)
-                    } else {
-                        Err((
-                            StatusCode::INTERNAL_SERVER_ERROR,
-                            format!("Unhandled internal error: {}", e),
-                        ))
-                    }
-                }))
-                .timeout(Duration::from_secs(10))
-                .layer(TraceLayer::new_for_http())
-                .into_inner()
-        )
-        .with_state(memory.clone());
+            .layer(HandleErrorLayer::new(|e: BoxError| async move {
+                if e.is::<tower::timeout::error::Elapsed>() {
+                    Ok(StatusCode::REQUEST_TIMEOUT)
+                } else {
+                    Err((
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        format!("Unhandled internal error: {}", e),
+                    ))
+                }
+            }))
+            .timeout(Duration::from_secs(10))
+            .layer(TraceLayer::new_for_http())
+            .into_inner()
+        );
+
+    let application_side_route = Router::new()
+        .route("/", get(address_request_handler))
+        .with_state(address_memory.clone())
+        .route("/", post(transaction_handler))
+        .with_state(payload_memory.clone())
+        .layer(
+            ServiceBuilder::new()
+            .layer(HandleErrorLayer::new(|e: BoxError| async move {
+                if e.is::<tower::timeout::error::Elapsed>() {
+                    Ok(StatusCode::REQUEST_TIMEOUT)
+                } else {
+                    Err((
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        format!("Unhandled internal error: {}", e),
+                    ))
+                }
+            }))
+            .timeout(Duration::from_secs(10))
+            .layer(TraceLayer::new_for_http())
+            .into_inner()
+        );
 
     tracing::info!("listening on {} for browser.", browser_side_addr);
     tracing::info!("listening on {} for application.", application_side_addr);
@@ -95,30 +125,73 @@ async fn main() -> Result<(), hyper::Error> {
     }
 }
 
-async fn browser_handler(State(memory): State<TempMemory>)
--> Sse<impl Stream<Item = Result<Event, Infallible>>>  {
-    let mem = memory.read().unwrap().to_owned();
-    let request = serde_json::to_string(&mem).unwrap();
-    *memory.write().unwrap() = RpcPayload::default();
-    
-    let stream = stream::repeat_with(move || Event::default().event("data").data(request.clone()))
+async fn payload_sse_handler(State(payload_memory): State<TempPayloadMemory>)
+-> Result<impl IntoResponse, StatusCode> {
+    if let Ok(mut mem) = payload_memory.lock() {
+        if let Some(payload) = mem.as_ref() {
+            if let Ok(text) = serde_json::to_string(payload) {
+                *mem = None;
+                return Ok(prepare_event("payload", text, 10))
+            }
+        }
+    }
+    Err(StatusCode::INTERNAL_SERVER_ERROR)
+}
+
+fn prepare_event(name: &'static str, text: String, interval: u64) 
+-> Sse<impl Stream<Item = Result<Event, Infallible>>> {
+    let stream = stream::repeat_with(move || 
+        Event::default().event(name).data(text.clone())
+    )
     .map(Ok)
-    .throttle(Duration::from_secs(5));
+    .throttle(Duration::from_secs(interval));
 
     Sse::new(stream).keep_alive(
         axum::response::sse::KeepAlive::new()
-            .interval(Duration::from_secs(5))
+            .interval(Duration::from_secs(interval))
             .text("keep-alive-text")
     )
 }
 
-async fn application_handler(State(memory): State<TempMemory>, Json(payload): Json<RpcPayload>)
+async fn address_fetch_handler(State(address_memory): State<TempAddressMemory>, Json(address): Json<Address>) 
 -> Result<impl IntoResponse, StatusCode> {
-    *memory.write().unwrap() = payload;
-
-    webbrowser::open("http://127.0.0.1:8080").unwrap();
+    let mut mem = address_memory.write().await;
+    *mem = Some(address);
     let res = RpcResponse{
         message: "Ok".to_string()
     }; 
     Ok(Json(res))
+}
+
+async fn transaction_handler(State(payload_memory): State<TempPayloadMemory>, Json(payload): Json<RpcPayload>)
+-> Result<impl IntoResponse, StatusCode> {
+    if let Ok(()) = webbrowser::open("http://127.0.0.1:8080/?payload=true") {
+        if let Ok(mut mem) = payload_memory.lock() {
+            *mem = Some(payload);
+            let res = RpcResponse{
+                message: "Ok".to_string()
+            }; 
+            return Ok(Json(res))
+        }
+    }
+    Err(StatusCode::INTERNAL_SERVER_ERROR)
+}
+
+async fn address_request_handler(State(address_memory): State<TempAddressMemory>) 
+-> Result<impl IntoResponse, StatusCode> {
+    if let Ok(()) = webbrowser::open("http://127.0.0.1:8080/?address=true") {
+        while address_memory.read().await.is_none() {
+            time::sleep(Duration::from_millis(100)).await;
+        }
+
+        let mut mem = address_memory.write().await;
+        if let Some(addr) = mem.as_ref() {
+            let res = RpcResponse{
+                message: addr.hex.clone()
+            }; 
+            *mem = None;
+            return Ok(Json(res))
+        }
+    }
+    Err(StatusCode::INTERNAL_SERVER_ERROR)
 }
